0354c7ee85d4dac77b545387c91b81cfe27223e3
Thiago <diasjuremathiago@gmail.com>
2024-10-27 13:33:35 -0300

    Update builderom.sh

--- builderom.sh
+++ builderom.sh
@@ -2,13 +2,13 @@ rm -rf .repo/local_manifests
 
 
 # repo init rom
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs --depth 1
 echo ==================
 echo Repo init success
 echo ==================
 
 # Local manifests
-git clone https://github.com/diasthiago11/local_manifest_odessa.git -b los21 .repo/local_manifests
+git clone https://github.com/diasthiago11/local_manifest_odessa.git -b android13 .repo/local_manifests
 echo ============================
 echo Local manifest clone success
 echo ============================

2f8b7ea4caa56712da0a39b27eee2eae5d77fd55
thz <diasjuremathiago@gmail.com>
2024-12-26 19:11:13 -0300

    Update builderom.sh

--- builderom.sh
+++ builderom.sh
@@ -7,7 +7,7 @@ echo Repo init success
 echo ==================
 
 # Local manifests
-git clone https://github.com/diasthiago11/local_manifest_odessa.git -b los21 .repo/local_manifests
+git clone https://github.com/thz22/local_manifest_odessa.git -b main .repo/local_manifests
 echo ============================
 echo Local manifest clone success
 echo ============================
@@ -24,6 +24,9 @@ export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=ture 
 echo ======= Export Done ======
 
+# Gapps
+git clone https://thjr5965@bitbucket.org/pops11s/vendor_gms_vic.git vendor/gms --depth 1
+
 # chmod
 chmod a+x device/motorola/odessa/applyPatches.sh
 
@@ -31,7 +34,7 @@ chmod a+x device/motorola/odessa/applyPatches.sh
 . build/envsetup.sh
 
 # Clone PrivateKeys
-git clone https://github.com/diasthiago11/keysz.git -b main vendor/lineage-priv/keys
+git clone https://github.com/thz22/keysz.git -b main vendor/lineage-priv/keys
 
 echo ====== Envsetup Done =======
 

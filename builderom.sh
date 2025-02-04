27a0a0b78e77f5c9e1e2610c3261687c929fd4aa
thz22 <thjr5965@gmail.com>
2025-02-04 16:29:18 -0300

    Update builderom.sh

--- builderom.sh
+++ builderom.sh
@@ -1,13 +1,13 @@
 rm -rf .repo/local_manifests
 
 # repo init rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
 echo ==================
 echo Repo init success
 echo ==================
 
 # Local manifests
-git clone https://github.com/thz22/local_manifest_odessa.git -b main .repo/local_manifests
+git clone https://github.com/thz22/local_manifest_odessa.git -b pixel .repo/local_manifests
 echo ============================
 echo Local manifest clone success
 echo ============================
@@ -34,7 +34,7 @@ chmod a+x device/motorola/odessa/applyPatches.sh
 rm -rf build/soong
 
 # Clone O3
-git clone https://github.com/thz22/android_build_soong.git -b lineage-21.0 build/soong
+git clone https://github.com/thz22/build_soong.git -b fourteen build/soong
 
 # Set up build environment
 . build/envsetup.sh
